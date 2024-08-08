446dbc0d5e4f18140ffe14e8d22f9b00b6d3af3f
Salman Ali <salmanafridi945@gmail.com>
2024-08-08 22:22:52 +0500

    added html file and basic css i will update it later

---
+++ main.js
@@ -0,0 +1,22 @@
+const quizData = [
+    {
+        question: "HTML stand for ?",
+        options: [
+            "HyperText Markup Language",
+            "Hyper Transfer markeup language",
+            "Hyper Transfer makeup language",
+            "Hyper transport markeup language",
+        ],
+        correct: 0,
+    },
+    {
+        question: "How many days makes a week ?",
+        options: [
+            "8",
+            "7",
+            "5",
+            "3",
+        ],
+        correct: 1,
+    }
+]
